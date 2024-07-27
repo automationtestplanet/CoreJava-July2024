31f8b954b7d5f28db0f767a67b030f765540a8af
PremArya1440 <147743962+PremArya1440@users.noreply.github.com>
2024-07-28 00:51:42 +0530

    Update Assignment.java

--- src/assignments/prem/Assignment.java
+++ src/assignments/prem/Assignment.java
@@ -1,14 +1,31 @@
-package assignments.prem;
-
-public class Assignment {
-	
-	// write reusable program to revere a string without changing special characters position
+// write reusable program to revere a string without changing special characters position
 	// ex: String str = "he@l#&l%o"  -> o@l#&le%h
-	
-
+package assignments.prem;
+public class Assignment_one {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static String reverseStringSpecialChar((String str) {
+        char[] chars = str.toCharArray();
+        int left = 0, right = chars.length - 1;
+        while (left < right) {
+            if (!Character.isLetter(chars[left])) {
+                left++;
+            } else if (!Character.isLetter(chars[right])) {
+                right--;
+            } else {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new String(chars);
+    }
+    public static void main(String[] args) {
+        String input = "he@l#&l%o";
+        String result = reverseStringSpecialChar(input);
+        System.out.println(result); 
+    }
+}
 
 }
